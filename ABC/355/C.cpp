297e6212fed0fe0d1a7ef8956133ee41bfa7c96b
Ryota-Genseki <s225180y@st.go.tuat.ac.jp>
2024-06-01 23:28:28 +0900

    add 356

--- ABC/355/C.cpp
+++ ABC/355/C.cpp
@@ -6,20 +6,12 @@
 using namespace std;
 
 int main() {
-  int N;
-  long long int result=0, buf=0;
-  cin >> N;
-  vector<string> S(N);
-  // 入力
-  rep(i, 0, N)
-    cin >> S[i];
-  // 計算
-  rep(i, 0, N-1){
-    rep(j, i+1, N){
-      buf = stoll(A[i]+A[j]);
-      result += (buf%998244353);
+  int N, T;
+  cin >> N >> T;
+  vector<vector<bool>> grid(N, vector<bool> (N, false));
+  rep(i, 0, T){
+    rep(j, 0, N){
+      
     }
   }
-  // 出力
-  cout << (result%998244353) << endl;
 }
